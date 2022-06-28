93b4f52bb4c535d35d6f632882775733183f1594
Microsoft <1033718596@qq.com>
2022-06-28 10:31:37 -0700

    train small file

---
+++ Exps/run.sh
@@ -0,0 +1,25 @@
+# setup env
+export MMDET_DATASETS=/mnt/
+export OMP_NUM_THREADS=8
+
+# Test on existing model and checkpoint
+# refer for the website https://github.com/open-mmlab/mmdetection/blob/master/docs/en/1_exist_data_model.md
+# weightes are downloaded from https://github.com/open-mmlab/mmdetection/tree/master/configs/faster_rcnn and save into checkpoints
+python tools/test.py\
+ configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py \
+ checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth \
+ --show \
+ --work-dir=results \
+ --eval="bbox" \
+
+python tools/train.py \
+    configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py \
+    --auto-scale-lr \
+    --cfg-options auto_scale_lr.base_batch_size=12 data.workers_per_gpu=8 data.samples_per_gpu=12 log_config.interval=30000 \
+    --work-dir=/mnt/cps/coco/
+
+
+    
+
+
+
--- Exps/set_env.sh
+++ Exps/set_env.sh
@@ -2,3 +2,4 @@ pip install mmcv-full
 pip install -r requirements/build.txt
 pip install "git+https://github.com/open-mmlab/cocoapi.git#subdirectory=pycocotools"
 pip install -v -e .  # or "python setup.py develop"
+pip install pycocotools
