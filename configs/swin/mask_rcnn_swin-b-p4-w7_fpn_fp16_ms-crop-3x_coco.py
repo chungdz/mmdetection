84327086887fa7d26fd54e8f9aabb1e7bc3788b9
Microsoft <1033718596@qq.com>
2022-06-30 18:49:51 -0700

    add base parameters

---
+++ configs/swin/mask_rcnn_swin-b-p4-w7_fpn_fp16_ms-crop-3x_coco.py
@@ -0,0 +1,13 @@
+_base_ = './mask_rcnn_swin-t-p4-w7_fpn_fp16_ms-crop-3x_coco.py'
+pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_base_patch4_window7_224_22k.pth'  # noqa
+model = dict(
+    backbone=dict(
+        embed_dim=128,
+        depths=[2, 2, 18, 2],
+        num_heads=[4, 8, 16, 32],
+        window_size=7,
+        mlp_ratio=4.,
+        drop_rate=0.,
+        attn_drop_rate=0.,
+        drop_path_rate=0.3,
+        init_cfg=dict(type='Pretrained', checkpoint=pretrained)))
--- exps/run.sh
+++ exps/run.sh
@@ -1,6 +1,7 @@
 # setup env
 export MMDET_DATASETS=/mnt/coco/
 export OMP_NUM_THREADS=8
+python -m exps.make_smaller_coco
 
 # Test on existing model and checkpoint
 # refer for the website https://github.com/open-mmlab/mmdetection/blob/master/docs/en/1_exist_data_model.md
@@ -12,8 +13,6 @@ python tools/test.py\
  --work-dir=results \
  --eval="bbox" \
 # train on smaller files for better debug
-python -m exps.make_smaller_coco
-
 python tools/train.py \
     configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py \
     --auto-scale-lr \
@@ -35,6 +34,44 @@ python tools/train.py \
                     runner.max_epochs=2 \
     --work-dir=cps
 
+# run swin
+# Test on existing model and checkpoint
+# refer for the website https://github.com/open-mmlab/mmdetection/blob/master/docs/en/1_exist_data_model.md
+# weightes are downloaded from https://github.com/open-mmlab/mmdetection/tree/master/configs/faster_rcnn and save into checkpoints
+python tools/test.py\
+ configs/swin/mask_rcnn_swin-s-p4-w7_fpn_fp16_ms-crop-3x_coco.py \
+ checkpoints/mask_rcnn_swin-s-p4-w7_fpn_fp16_ms-crop-3x_coco_20210903_104808-b92c91f1.pth \
+ --show \
+ --work-dir=results \
+ --eval bbox segm\
+
+# train on smaller files and smaller swin for better debug
+# download https://github.com/SwinTransformer/storage/releases/download/v1.0.0/checkpoints/swin_small_patch4_window7_224.pth
+# into checkpoints
+python tools/train.py \
+    configs/faster_rcnn/mask_rcnn_swin-s-p4-w7_fpn_fp16_ms-crop-3x_coco.py \
+    --auto-scale-lr \
+    --cfg-options auto_scale_lr.base_batch_size=12 \
+                    data.workers_per_gpu=8 \
+                    data.samples_per_gpu=12 \
+                    log_config.interval=100 \
+                    runner.max_epochs=2 \
+                    data.train.ann_file='/mnt/coco/annotations/instances_train2017.small.json' \
+                    pretrained='checkpoints/swin_small_patch4_window7_224.pth'
+    --work-dir=cps
+
+# train on smaller files and swin base for better debug
+python tools/train.py \
+    configs/faster_rcnn/mask_rcnn_swin-b-p4-w7_fpn_fp16_ms-crop-3x_coco.py \
+    --auto-scale-lr \
+    --cfg-options auto_scale_lr.base_batch_size=12 \
+                    data.workers_per_gpu=8 \
+                    data.samples_per_gpu=12 \
+                    log_config.interval=100 \
+                    runner.max_epochs=2 \
+                    data.train.ann_file='/mnt/coco/annotations/instances_train2017.small.json' \
+                    pretrained='checkpoints/swin_base_patch4_window7_224_22k.pth'
+    --work-dir=cps
 
     
 
