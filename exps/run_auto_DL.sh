bee70c9848878e49f6611fb74a870d2641bf0584
xiaoxin Hu <1033718596@qq.com>
2022-07-08 14:59:32 -0700

    no fpn

--- exps/run_auto_DL.sh
+++ exps/run_auto_DL.sh
@@ -120,11 +120,42 @@ python tools/train.py \
                     data.workers_per_gpu=8 \
                     data.samples_per_gpu=3 \
                     log_config.interval=1000 \
-                    runner.max_epochs=10 \
+                    runner.max_epochs=5 \
                     model.backbone.init_cfg.checkpoint='/root/autodl-tmp/checkpoints/swin_base_patch4_window7_224_22k.pth'\
-                    load_from='/root/autodl-tmp/cps/swin_based_rcnn.0703.pth' \
+                    load_from='/root/autodl-tmp/cps/second_swin_b_rcnn.pth' \
                     evaluation.interval=1 \
-                    lr_config.step="[7,10]" \
+                    lr_config.step="[4,5]" \
+                    lr_config.warmup_iters=1 \
+    --work-dir=/root/autodl-tmp/cps
+
+./tools/dist_train.sh \
+    configs/swin/mask_rcnn_swin-s-p4-w7_fpn_fp16_ms-crop-3x_coco.py \
+    6 \
+    --auto-scale-lr \
+    --cfg-options auto_scale_lr.base_batch_size=18 \
+                    data.workers_per_gpu=8 \
+                    data.samples_per_gpu=3 \
+                    log_config.interval=1000 \
+                    runner.max_epochs=5 \
+                    model.backbone.init_cfg.checkpoint='/root/autodl-tmp/checkpoints/swin_small_patch4_window7_224.pth'\
+                    load_from='/root/autodl-tmp/cps/swin_small_mask_rcnn.0704.ep15.pth' \
+                    evaluation.interval=1 \
+                    lr_config.step="[4,5]" \
+                    lr_config.warmup_iters=1 \
+    --work-dir=/root/autodl-tmp/cps
+
+./tools/dist_train.sh \
+    configs/swin/mask_rcnn_swin-b-p4-w7_fp16_ms-crop-3x_coco.py \
+    6 \
+    --auto-scale-lr \
+    --cfg-options auto_scale_lr.base_batch_size=18 \
+                    data.workers_per_gpu=8 \
+                    data.samples_per_gpu=3 \
+                    log_config.interval=1000 \
+                    runner.max_epochs=22 \
+                    model.backbone.init_cfg.checkpoint='/root/autodl-tmp/checkpoints/swin_base_patch4_window7_224_22k.pth'\
+                    evaluation.interval=1 \
+                    lr_config.step="[19,22]" \
                     lr_config.warmup_iters=1 \
     --work-dir=/root/autodl-tmp/cps
 
@@ -164,6 +195,22 @@ python tools/train.py \
 
 
     
+# no fpn
 
+./tools/dist_train.sh \
+    configs/swin/mask_rcnn_swin-b-p4-w7_fp16_ms-crop-3x_coco.py \
+    6 \
+    --auto-scale-lr \
+    --cfg-options auto_scale_lr.base_batch_size=18 \
+                    data.workers_per_gpu=8 \
+                    data.samples_per_gpu=3 \
+                    log_config.interval=1000 \
+                    runner.max_epochs=2 \
+                    data.train.ann_file='/root/autodl-tmp/coco/annotations/instances_train2017.small.json' \
+                    model.backbone.init_cfg.checkpoint='/root/autodl-tmp/checkpoints/swin_base_patch4_window7_224_22k.pth'\
+                    evaluation.interval=1 \
+                    lr_config.step="[19,22]" \
+                    lr_config.warmup_iters=1 \
+    --work-dir=/root/autodl-tmp/cps
 
 
