0e7fbf49e89492b385fed2aa8eb1ac851869f98f
xiaoxin Hu <1033718596@qq.com>
2022-07-16 19:35:43 -0700

    add gem pooling

--- mmdet/models/roi_heads/roi_extractors/__init__.py
+++ mmdet/models/roi_heads/roi_extractors/__init__.py
@@ -2,5 +2,6 @@
 from .base_roi_extractor import BaseRoIExtractor
 from .generic_roi_extractor import GenericRoIExtractor
 from .single_level_roi_extractor import SingleRoIExtractor
+from .single_level_roi_extractor_gem import SingleRoIExtractorGeM
 
-__all__ = ['BaseRoIExtractor', 'SingleRoIExtractor', 'GenericRoIExtractor']
+__all__ = ['BaseRoIExtractor', 'SingleRoIExtractor', 'GenericRoIExtractor', 'SingleRoIExtractorGeM']
